9f8ebf394980123b86b78c0f1954570fc94fee92
Téo Goulois <teo.goulois.dev@gmail.com>
2024-02-17 23:36:49 +0100

    🎉 init projecet

---
+++ src/config/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Title",
+  description: "Description",
+};
---
+++ src/config/viewport.ts
@@ -0,0 +1,5 @@
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  themeColor: "black",
+};
