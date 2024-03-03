3035b130c4e7c9d83a5fcbf3051fbdb4de8583bd
Téo Goulois <teo.goulois.dev@gmail.com>
2024-03-03 01:00:40 +0100

    🐛 Add globalThis.crypto and import crypto from node:crypto

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import crypto from "node:crypto"
+globalThis.crypto ??= crypto.webcrypto
 const nextConfig = {
     webpack: (config, { webpack }) => {
         config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
